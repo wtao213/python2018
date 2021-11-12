0cb611bcca4f406fa7bdd24157adfd43622259f6
Tao <wanting.tao@gmail.com>
2021-11-11 21:31:52 -0500

    Add files via upload

--- 841A2q1.py
+++ 841A2q1.py
@@ -31,6 +31,7 @@ df = rpc.groupby(['Dialer Id'],as_index=False).agg(
         
         'PTP': [sum],
         'Sum of Payment Amount': ['mean',sum],
+        'Sum of Promise Amount':['mean'],
         'Broken': [sum],
         'Closed': [sum],
         'Open': [sum],
@@ -39,12 +40,18 @@ df = rpc.groupby(['Dialer Id'],as_index=False).agg(
 )
 
 df = df.droplevel(1, axis=1)  
-df= df.set_axis(['Dialer Id', 'N', 'Number of RPC','Number of PTP','Average Payment Amount','Sum of Payment Amount','Broken','Closed','Open','Kept'], axis=1)
+df= df.set_axis(['Dialer Id', 'N', 'Number of RPC','Number of PTP','Average Payment Amount','Total Payment Amount','Average Promise Amount','Broken','Closed','Open','Kept'], axis=1)
 
 
 df['PTP/RPC%'] =  df['Number of PTP']*100 / df['Number of RPC']
 df['%of promises Kept'] = df['Kept'] *100 / (df['Kept'] + df['Broken'] )
 
+#
+df2= df.loc[:,['Dialer Id','Number of RPC','Number of PTP','PTP/RPC%','Average Promise Amount','%of promises Kept','Total Payment Amount']]
+df2= df2.sort_values(by=['Total Payment Amount'],ascending=False)
+df2.to_csv(r"E:\MMA\MMA841 Ops\A2\Q1_2.csv",index=False,sep=',')
+
+del df2
 
 
 ############################
@@ -54,6 +61,22 @@ time['RPC_rate'] = time['Right Party Connect Count'] / time['Call Count']
 
 
 time['RPC_rate'].describe()
+
+time['RPC_rate'].mean()
+time['RPC_rate'].std()
+
+##
+UCL = time['RPC_rate'].mean() + 3*time['RPC_rate'].std()
+LCL = time['RPC_rate'].mean() - 3*time['RPC_rate'].std()
+
+
+UCL
+LCL
+
+# 7 reps out of the boundry, which is 4.69%
+time.loc[(time['RPC_rate'] > UCL) | (time['RPC_rate'] < LCL)]
+
+
 
 # can be use either in 3) or 4)
 plt.hist(time['RPC_rate'], bins=40, range = (0, 0.2),color = "skyblue",ec='blue')
@@ -76,6 +99,26 @@ plt.show()
 df2 = pd.merge(df,time,left_on='Dialer Id',right_on='Dialer ID', how='right')
 
 df2.columns
+
+
+#Call Count
+df2['Call Count'].describe()
+
+
+plt.hist(df2['Call Count'], bins=50,color = "skyblue",ec='blue')
+plt.title("Dialer's Call Count Distribution")
+plt.xlabel("Call Count")
+plt.ylabel("Dialer count")
+# add in vertial line
+plt.vlines(x=2346,  colors='red', ls=':',ymin=0, ymax=15,lw=2, label='Call Count Average = 2346')
+plt.legend( loc='upper right')
+plt.show()
+
+
+
+
+
+
 # promise rate for dialer
 df['PTP/RPC%'].describe()
 
@@ -96,9 +139,9 @@ plt.show()
 
 
 # Payment vs Call Count
-plt.plot(df2['Sum of Payment Amount'], df2['Call Count'], 'o', color='lightblue')
+plt.plot(df2['Total Payment Amount'], df2['Call Count'], 'o', color='lightblue')
 plt.title("Payment vs Call Count")
-plt.xlabel("Sum of Payment Amount $")
+plt.xlabel("Total Payment Amount $")
 plt.ylabel("Call Count")
 
 
@@ -111,9 +154,9 @@ plt.ylabel("Call Count")
 
 
 # Payment vs Number of RPC
-plt.plot(df2['Sum of Payment Amount'], df2['Number of RPC'], 'o', color='lightblue')
+plt.plot(df2['Total Payment Amount'], df2['Number of RPC'], 'o', color='lightblue')
 plt.title("Payment vs Number of RPC")
-plt.xlabel("Sum of Payment Amount $")
+plt.xlabel("Total Payment Amount $")
 plt.ylabel("Number of RPC")
 
 
